08ede721bf82b49aa6154943794de7297724288f
Akshit545 <akshitakttyagi77889@gmail.com>
2022-02-12 11:16:07 +0530

    making index.js

---
+++ home.js
@@ -0,0 +1,29 @@
+$('#desc-input').focus(function () {
+  $('#desc-input').css('backgroundColor', 'lightgrey');
+  $('#desc').css('backgroundColor', 'lightgrey');
+});
+
+$('#desc-input').blur(function () {
+  $('#desc').css('backgroundColor', 'white');
+  $('#desc-input').css('backgroundColor', 'white');
+});
+
+$('#category').focus(function () {
+  $('#cate').css('backgroundColor', 'lightgrey');
+  $('#category').css('backgroundColor', 'lightgrey');
+});
+
+$('#category').blur(function () {
+  $('#cate').css('backgroundColor', 'white');
+  $('#category').css('backgroundColor', 'white');
+});
+
+$('#duedate').focus(function () {
+  $('#date').css('backgroundColor', 'lightgrey');
+  $('#duedate').css('backgroundColor', 'lightgrey');
+});
+
+$('#duedate').blur(function () {
+  $('#date').css('backgroundColor', 'white');
+  $('#duedate').css('backgroundColor', 'white');
+});
